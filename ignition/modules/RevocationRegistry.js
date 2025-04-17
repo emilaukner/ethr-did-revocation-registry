53782f1e371619d29b440373b8c43a6f9b4fbe95
emilaukner <emilaukner@gmail.com>
2025-04-17 12:17:07 +0200

    Redeployed contract

--- ignition/modules/RevocationRegistry.js
+++ ignition/modules/RevocationRegistry.js
@@ -1,11 +1,11 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const { buildModule } = require('@nomicfoundation/hardhat-ignition/modules');
 
 const RevocationRegistryModule = buildModule(
-  "RevocationRegistryModule",
-  (m) => {
-    const registry = m.contract("CredentialRevocationRegistry");
-    return { registry };
-  }
+	'CredentialRevocationRegistry',
+	(m) => {
+		const registry = m.contract('CredentialRevocationRegistry');
+		return { registry };
+	}
 );
 
 module.exports = RevocationRegistryModule;
